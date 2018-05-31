c83002b723b6025051292a55c2f74d14f1f937ea
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2018-05-31 20:42:50 +0100

    Update bme280-microdot.py

--- bme280-microdot.py
+++ bme280-microdot.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import bme280
 import time
+import sys
 from microdotphat import write_string, set_decimal, clear, show
 
 try:
     while True:
-        clear()
-        temperature = weather.temperature()
-        write_string( "%.2f" % temperature + "C", kerning=False)
+	     clear()
+        temperature,pressure,humidity = bme280.readBME280All()
+        write_string( "%.2f" % temperature + "°C", kerning=False)
         show()
-        time.sleep(1)
+        time.sleep(5)
+        clear()
+        temperature,pressure,humidity = bme280.readBME280All()
+        write_string( "%.2f" % pressure + "hPa", kerning=False)
+        show()
+        time.sleep(5)
+        temperature,pressure,humidity = bme280.readBME280All()
+        write_string( "%.2f" % humidity + "%", kerning=False)
+        show()
+        time.sleep(5)
 
 except KeyboardInterrupt:
+    print "\n"
     pass

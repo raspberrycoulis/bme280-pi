9e57a3eb1be121d7fb2f8c6f679a6cb15197e0f1
Wesley Archer <wesleyarcher@gmail.com>
2018-06-01 21:57:09 +0100

    Timing and decimalisation

--- bme280-microdot-temp.py
+++ bme280-microdot-temp.py
@@ -3,16 +3,15 @@
 import bme280
 import time
 import sys
-from microdotphat import write_string, set_decimal, clear, show, scroll
+from microdotphat import write_string, set_decimal, clear, show
 
 try:
     while True:
         clear()
         temperature,pressure,humidity = bme280.readBME280All()
-        write_string( "%.1f" % temperature + "C", kerning=False)
+        write_string( "%.2f" % temperature + "C", kerning=False)
         show()
-        scroll()
-        time.sleep(5)
+        time.sleep(1.0)
 
 except KeyboardInterrupt:
     print "\n"

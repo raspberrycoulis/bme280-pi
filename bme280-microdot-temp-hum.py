3fbb5a0a9a16323bdfbabdbcc87521dec0e6014b
Wesley Archer <wesleyarcher@gmail.com>
2018-06-01 21:32:44 +0100

    fixed bme280

--- bme280-microdot-temp-hum.py
+++ bme280-microdot-temp-hum.py
@@ -8,7 +8,7 @@ from microdotphat import write_string, set_decimal, clear, show
 try:
     while True:
         clear()
-        temperature,humidity = bme280.readBME280All()
+        temperature,pressure,humidity = bme280.readBME280All()
         write_string( "%.1f" % temperature + "C", kerning=False)
         show()
         time.sleep(5)

5b4a710cc536276b0e9e159875bd989ab7807514
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2018-06-02 15:18:36 +0100

    Comments added for help

--- bme280-microdot-beebotte.py
+++ bme280-microdot-beebotte.py
@@ -7,21 +7,23 @@ import threading
 from microdotphat import write_string, set_decimal, clear, show
 from beebotte import *
 
-### Replace CHANNEL_TOKEN with that of your channel
+### Replace CHANNEL_TOKEN with that of your Beebotte channel
 bbt = BBT(token = 'CHANNEL_TOKEN')
 
-### Change channel name as suits you - in this instance, it is called BME280.
+### Change Beebotte channel name as suits you - in this instance, it is called BME280.
 temp_resource   = Resource(bbt, 'BME280', 'temperature')
 pressure_resource  = Resource(bbt, 'BME280', 'pressure')
 humidity_resource = Resource(bbt, 'BME280', 'humidity')
 
+# Sends data to your Beebotte channel
 def beebotte():
     while True:
         temp_resource.write(round(temperature,1))
         pressure_resource.write(round(pressure,0))
         humidity_resource.write(round(humidity,0))
-        time.sleep(900)
+        time.sleep(900)    # 15 mins to prevent maxing API limit
 
+# Display stats on the Micro Dot pHAT
 def microdot():
     clear()
     write_string( "%.1f" % temperature + "C", kerning=False)
@@ -37,13 +39,17 @@ def microdot():
     time.sleep(5)
 
 try:
+    # Get the first reading from the BME280 sensor
     temperature,pressure,humidity = bme280.readBME280All()
+    # Start the Beebotte function as a thread so it works in the background
     beebotte_thread = threading.Thread(target=beebotte)
     beebotte_thread.start()
+    # Run a loop to collect data and display it on the Micro Dot pHAT
     while True:
         temperature,pressure,humidity = bme280.readBME280All()
         microdot()
 
+# Attempt to exit cleanly - not quite there, needs work!
 except (KeyboardInterrupt, SystemExit):
     print "\n"
     sys.exit()

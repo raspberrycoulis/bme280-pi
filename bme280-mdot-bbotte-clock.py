c4e57febe030c94fb532d39032f51793ace4cbd3
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2018-06-07 13:38:07 +0100

    Kill thread when exiting
    Kill the thread when exiting with CTRL+C / `KeyboardInterrupt` if running in terminal.

--- bme280-mdot-bbotte-clock.py
+++ bme280-mdot-bbotte-clock.py
@@ -87,6 +87,7 @@ try:
     temperature,pressure,humidity = bme280.readBME280All()
     # Start the Beebotte function as a thread so it works in the background
     beebotte_thread = threading.Thread(target=beebotte)
+    beebotte_thread.daemon = True
     beebotte_thread.start()
     # Run a loop to collect data and display it on the Micro Dot pHAT
     # and send notifications via Pushover if it is too hot
